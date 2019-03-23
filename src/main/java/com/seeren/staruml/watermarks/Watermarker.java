cf1e8692bfd950240bd585be8057742f91649ade
seeren <consultant@seeren.fr>
2019-03-23 23:17:09 +0100

    Add watermarker

---
+++ src/main/java/com/seeren/staruml/watermarks/Watermarker.java
@@ -0,0 +1,22 @@
+package com.seeren.staruml.watermarks;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class Watermarker {
+
+	public final Document getDocument(final String fileName) throws IllegalArgumentException {
+		try {
+			return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(fileName));
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			throw new IllegalArgumentException("Unable to read " + fileName);
+		}
+	}
+
+}
