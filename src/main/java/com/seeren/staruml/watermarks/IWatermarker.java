a458b580d156960261cb0371083ef46b21f1f0cc
seeren <consultant@seeren.fr>
2019-03-23 23:16:54 +0100

    Add watermark interface

---
+++ src/main/java/com/seeren/staruml/watermarks/IWatermarker.java
@@ -0,0 +1,11 @@
+package com.seeren.staruml.watermarks;
+
+import java.io.File;
+
+public interface IWatermarker {
+
+	public void mark(final File file) throws RuntimeException;
+
+	public void unmark(final File file) throws RuntimeException;
+
+}
