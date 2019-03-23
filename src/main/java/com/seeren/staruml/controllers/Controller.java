ad846b5fb737d3b64a728c40c152480162eb9b01
seeren <consultant@seeren.fr>
2019-03-23 23:16:03 +0100

    Map options

--- src/main/java/com/seeren/staruml/controllers/Controller.java
+++ src/main/java/com/seeren/staruml/controllers/Controller.java
@@ -1,19 +1,19 @@
 package com.seeren.staruml.controllers;
 
-import java.util.List;
+import java.util.Map;
 
 import com.seeren.staruml.loggers.Logger;
 
 public abstract class Controller {
 
-	public final List<String> input;
+	public final Map<String, String> options;
 
 	public final Logger logger;
 
 	abstract public void execute() throws RuntimeException;
 
-	public Controller(List<String> input, Logger logger) {
-		this.input = input;
+	public Controller(final Map<String, String> options, final Logger logger) {
+		this.options = options;
 		this.logger = logger;
 	}
 
