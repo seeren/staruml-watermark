68b50ad1defb4e45fc7e3af9fba2c8c4476cacde
seeren <consultant@seeren.fr>
2019-07-17 22:45:35 +0200

    Use UTF8 output

--- src/main/java/com/seeren/staruml/watermarks/SVGWatermarker.java
+++ src/main/java/com/seeren/staruml/watermarks/SVGWatermarker.java
@@ -1,8 +1,10 @@
 package com.seeren.staruml.watermarks;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,30 +19,31 @@ import org.w3c.dom.NodeList;
 
 public final class SVGWatermarker extends Watermarker implements IWatermarker {
 
-	@Override
-	public final void mark(final File file) throws RuntimeException {
-	}
+    @Override
+    public final void mark(final File file) throws RuntimeException {
+    }
 
-	@Override
-	public final void unmark(final File file) throws RuntimeException {
-		try {
-			List<Node> watermarks = new ArrayList<Node>();
-			Document document = getDocument(file.getAbsolutePath());
-			NodeList childNodes = document.getElementsByTagName("text");
-			for (int i = 0; i < childNodes.getLength(); i++) {
-				if (childNodes.item(i).getTextContent().contentEquals("UNREGISTERED")) {
-					watermarks.add(childNodes.item(i));
-				}
-			}
-			if (0 == watermarks.size()) {
-				throw new RuntimeException("Document is not watermarked");
-			}
-			watermarks.forEach(watermark -> watermark.getParentNode().removeChild(watermark));
-			TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document),
-					new StreamResult(new FileWriter(new File(file.getAbsolutePath()))));
-		} catch (TransformerException | IOException e) {
-			throw new RuntimeException(e.getMessage());
+    @Override
+    public final void unmark(final File file) throws RuntimeException {
+	try {
+	    List<Node> watermarks = new ArrayList<Node>();
+	    Document document = getDocument(file.getAbsolutePath());
+	    NodeList childNodes = document.getElementsByTagName("text");
+	    for (int i = 0; i < childNodes.getLength(); i++) {
+		if (childNodes.item(i).getTextContent().contentEquals("UNREGISTERED")) {
+		    watermarks.add(childNodes.item(i));
 		}
+	    }
+	    if (0 == watermarks.size()) {
+		throw new RuntimeException("Document is not watermarked");
+	    }
+	    watermarks.forEach(watermark -> watermark.getParentNode().removeChild(watermark));
+	    TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document),
+		    new StreamResult(new OutputStreamWriter(new FileOutputStream(new File(file.getAbsolutePath())),
+			    StandardCharsets.UTF_8)));
+	} catch (TransformerException | IOException e) {
+	    throw new RuntimeException(e.getMessage());
 	}
+    }
 
 }
